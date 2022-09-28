77d778128ed49273bf764e093219c97bf62aa924
zhubinwu <zhubinwu@binwudeMacBook-Pro.local>
2022-09-28 23:12:50 +0800

    update

--- simulateImage.py
+++ simulateImage.py
@@ -4,18 +4,17 @@ imx = 2048
 imy = 2048
 kernel_x = 35
 kernel_y = 35
-kernel_num = 24
+# kernel_num = 24
 
-def compute_image(m_cmask):
-    pass
 
-def shift(m_cmask):
-    shift_cmask = torch.zeros(m_cmask.size(), dtype=torch.complex64)
-    shift_cmask[:imx//2, :imy//2] = m_cmask[imx//2:, imy//2:]     # 1 = 4
-    shift_cmask[:imx//2, imy//2:] = m_cmask[imx//2:, :imy//2]     # 2 = 3
-    shift_cmask[imx//2:, :imy//2] = m_cmask[:imx//2, imy//2:]     # 3 = 2
-    shift_cmask[imx//2:, imy//2:] = m_cmask[:imx//2, :imy//2]     # 4 = 1
+def shift(cmask):
+    shift_cmask = torch.zeros(cmask.size(), dtype=torch.complex64)
+    shift_cmask[:imx//2, :imy//2] = cmask[imx//2:, imy//2:]     # 1 = 4
+    shift_cmask[:imx//2, imy//2:] = cmask[imx//2:, :imy//2]     # 2 = 3
+    shift_cmask[imx//2:, :imy//2] = cmask[:imx//2, imy//2:]     # 3 = 2
+    shift_cmask[imx//2:, imy//2:] = cmask[:imx//2, :imy//2]     # 4 = 1
     return shift_cmask
+
 
 def kernel_mult(knx, kny, kernel, mask_fft):
     imxh = imx//2
@@ -26,31 +25,41 @@ def kernel_mult(knx, kny, kernel, mask_fft):
     output[xoff:xoff+knx, yoff:yoff+kny] = torch.mul(kernel, mask_fft[xoff:xoff+knx, yoff:yoff+kny])
     return output
 
-if __name__ == "__main__":
-    # background_real = 1.0
-    # background_img = 0.0
-    dose = 1.0
-    scale = torch.randn((kernel_num, ), dtype=torch.float64)
-    print(scale.size())
-    kernel = torch.zeros((kernel_x, kernel_y, kernel_num), dtype=torch.complex64)
-    m_mask = torch.randn((imx, imy), dtype=torch.complex64)
-    # print(m_mask)
-    # m_cmask = torch.zeros((imx, imy))
-    # m_cmask[:, :] = m_mask
-    m_mask = shift(m_mask)
-    print(m_mask.size())
-    m_fftmask = torch.fft.fft2(m_mask, norm="forward")
-    m_fftmask = shift(m_fftmask)
-    mul_fft = torch.zeros(m_fftmask.size(), dtype=torch.complex64)
+
+def compute_image(cmask, kernel, scale, workx, worky, dose, kernel_level):
+    kernel_num = kernel_level
+    cmask = shift(cmask)
+    cmask_fft = torch.fft.fft2(cmask, norm="forward")
+    cmask_fft = shift(cmask_fft)
+    mul_fft = torch.zeros(cmask_fft.size(), dtype=torch.complex64)
     for i in range(kernel_num):
-        # temp = kernel_mult(kernel_x, kernel_y, kernel[:, :, i], m_fftmask)
-        temp = shift(kernel_mult(kernel_x, kernel_y, kernel[:, :, i], m_fftmask))
+        temp = shift(kernel_mult(kernel_x, kernel_y, kernel[:, :, i], cmask_fft))
         # temp = torch.fft.ifft2(temp)
-        temp = shift(torch.fft.ifft2(temp))
+        temp = shift(torch.fft.ifft2(temp, norm="forward"))
         print(torch.abs(temp).size())
         mul_fft += scale[i] * torch.pow(torch.abs(temp), 2)
-        # print(torch.pow(torch.abs(temp), 2).size())
-        # break
+    return mul_fft
 
-    # m_mask = torch.randn((imx, imy))
-    # m_fftmask = torch.fft.rfft2(m_mask, norm="forward")
+
+def mask_float(mask, dose):
+    return (dose * mask).to(torch.complex64)
+
+
+def simulate_image(mask, kernel_type, dose, kernel_level):
+    kernel_num = kernel_level
+    if kernel_type == 0:
+        kernel = torch.randn((kernel_x, kernel_y, kernel_num), dtype=torch.complex64)
+        scale = torch.randn((kernel_num,), dtype=torch.float64)
+    cmask = mask_float(mask, dose)
+    image = compute_image(cmask, kernel, scale, 0, 0, dose, kernel_level)
+
+    return image
+
+
+if __name__ == "__main__":
+    mask = torch.randn((imx, imy), dtype=torch.float64)
+    # kernel_types = [""]
+    dose = 1.0
+    kernel_level = 15
+    output = simulate_image(mask, 0, dose, kernel_level)
+    # print(output)
