find the bug is mostly because of the calculation precision

ilt/opc.py
@@ -15,8 +15,9 @@ from constant import ZERO_ERROR, WEIGHT_EPE_REGION
 from constant import device
 from hammer import Hammer
 from sraf import Sraf
-from utils import is_pixel_on
+from utils import is_pixel_on, unit_test_params
 from eval import EpeChecker
+
 
 class OPC(object):
 
@@ -82,7 +83,7 @@ class OPC(object):
         self.initialize_params()
         self.update_mask()
         self.determine_epe_weight()
-        self.m_epe_samples = self.m_epe_cheker.find_sample_point()
+        # self.m_epe_samples = self.m_epe_cheker.find_sample_point()
 
     def initialize_mask(self):
         self.m_mask[LITHOSIM_OFFSET:MASK_TILE_END_Y, LITHOSIM_OFFSET:MASK_TILE_END_X] = \
@@ -107,6 +108,7 @@ class OPC(object):
         temp_params[self.m_mask[LITHOSIM_OFFSET:MASK_TILE_END_Y,
                                 LITHOSIM_OFFSET:MASK_TILE_END_X] < MASK_PRINTABLE_THRESHOLD] = -1
         self.m_params[LITHOSIM_OFFSET:MASK_TILE_END_Y, LITHOSIM_OFFSET:MASK_TILE_END_X] = temp_params
+        # unit_test_params(self.m_params, "/Users/zhubinwu/research/opc-hsd/cuilt/build/init_params.txt")
 
         # the above implementation is the same as the below one
         # temp = torch.zeros([OPC_TILE_Y, OPC_TILE_X], dtype=torch.float64)

ilt/simulate_image.py
@@ -1,6 +1,7 @@
 import torch
 import time
 from constant import device
+from utils import unit_test_kernel, unit_test_mask_fft
 
 imx = 2048
 imy = 2048
@@ -10,7 +11,7 @@ kernel_y = 35
 
 
 def shift(cmask):
-    shift_cmask = torch.zeros(cmask.size(), dtype=torch.complex64, device=device)
+    shift_cmask = torch.zeros(cmask.size(), dtype=torch.complex128, device=device)
     shift_cmask[:imx//2, :imy//2] = cmask[imx//2:, imy//2:]     # 1 = 4
     shift_cmask[:imx//2, imy//2:] = cmask[imx//2:, :imy//2]     # 2 = 3
     shift_cmask[imx//2:, :imy//2] = cmask[:imx//2, imy//2:]     # 3 = 2
@@ -23,19 +24,19 @@ def kernel_mult(knx, kny, kernel, mask_fft):
     imxy = imy//2
     xoff = imxh - knx//2
     yoff = imxy - kny//2
-    output = torch.zeros(mask_fft.size(), dtype=torch.complex64, device=device)
+    output = torch.zeros(mask_fft.size(), dtype=torch.complex128, device=device)
     output[xoff:xoff+knx, yoff:yoff+kny] = torch.mul(kernel, mask_fft[xoff:xoff+knx, yoff:yoff+kny])
     return output
 
 
-def expand_kernel(knx, kny, kernel, kernel_level):
-    imxh = imx // 2
-    imxy = imy//2
-    xoff = imxh - knx//2
-    yoff = imxy - kny//2
-    kernel_expand = torch.zeros((imx, imy, kernel_level), dtype=torch.complex64, device=device)
-    kernel_expand[xoff:xoff+knx, yoff:yoff+kny, :] = kernel
-    return kernel_expand
+# def expand_kernel(knx, kny, kernel, kernel_level):
+#     imxh = imx//2
+#     imxy = imy//2
+#     xoff = imxh - knx//2
+#     yoff = imxy - kny//2
+#     kernel_expand = torch.zeros((imx, imy, kernel_level), dtype=torch.complex128, device=device)
+#     kernel_expand[xoff:xoff+knx, yoff:yoff+kny, :] = kernel
+#     return kernel_expand
 
 
 # def compute_image1(cmask, kernel, scale, workx, worky, dose, kernel_level):
@@ -69,41 +70,66 @@ def expand_kernel(knx, kny, kernel, kernel_level):
 def compute_image(cmask, kernel, scale, workx, worky, dose, kernel_level):
     kernel_num = kernel_level
     cmask = shift(cmask)
-    cmask_fft = torch.fft.fft2(cmask)
+    cmask_fft = torch.fft.fft2(cmask, norm="forward")
     cmask_fft = shift(cmask_fft)
+    # unit_test_mask_fft(cmask_fft, "/Users/zhubinwu/research/opc-hsd/cuilt/build/init_fft_cmask.txt")
     mul_fft = torch.zeros(cmask_fft.size(), dtype=torch.float64, device=device)
+    print(kernel_num)
+    torch.set_printoptions(precision=8)
     for i in range(kernel_num):
-        temp = shift(kernel_mult(kernel_x, kernel_y, kernel[:, :, i], cmask_fft))
+        temp = kernel_mult(kernel_x, kernel_y, kernel[:, :, i], cmask_fft)
+        if i == 0:
+            print(temp[1024, 1024])
+        temp = shift(temp)
+        if i == 0:
+            print(temp[0, 0])
         # temp = torch.fft.ifft2(temp)
-        temp = shift(torch.fft.ifft2(temp))
+        temp = torch.fft.ifft2(temp, norm="forward")
+        if i == 0:
+            print(temp[0, 0])
+        temp = shift(temp)
         # print(torch.abs(temp).size())
         mul_fft += scale[i] * torch.pow(torch.abs(temp), 2)
+        if i == 0:
+            print(temp[1024, 1024])
     return mul_fft
+
 
 # 用dose作用mask
 def mask_float(mask, dose):
-    return (dose * mask).to(torch.complex64)
+    return (dose * mask).to(torch.complex128)
+
 
 def simulate_image(mask, kernel, scale, dose, kernel_level):
     # if kernel_type == 0:
     #     kernel = torch.randn((kernel_x, kernel_y, kernel_num), dtype=torch.complex64)
     #     scale = torch.randn((kernel_num,), dtype=torch.float64)
     cmask = mask_float(mask, dose)
+
+    # pixel_positive_1 = 0
+    # pixel_negative_1 = 0
+    # pixel_0 = 0
+    # for i in range(2048):
+    #     for j in range(2048):
+    #         if cmask[i, j] == cmask[548, 1224]:
+    #             pixel_negative_1 += 1
+    #         elif cmask[i, j] == cmask[613, 1250]:
+    #             pixel_positive_1 += 1
+    # print(pixel_negative_1, pixel_positive_1) # 1411056 227344
     image = compute_image(cmask, kernel, scale, 0, 0, dose, kernel_level)
     return image
 
-
-if __name__ == "__main__":
-    kernel_num = 15
-    mask = torch.randn((imx, imy), dtype=torch.float64)
-    kernel = torch.randn((kernel_x, kernel_y, kernel_num), dtype=torch.complex64)
-    scale = torch.randn((kernel_num,), dtype=torch.float64)
-    dose = 1.0
-    kernel_level = 15   #kernel数目
-    start = time.time()
-    output = simulate_image(mask, kernel, scale, dose, kernel_level)
-    end = time.time()
-    print(end-start)
+# if __name__ == "__main__":
+#     kernel_num = 15
+#     mask = torch.randn((imx, imy), dtype=torch.float64)
+#     kernel = torch.randn((kernel_x, kernel_y, kernel_num), dtype=torch.complex128)
+#     scale = torch.randn((kernel_num,), dtype=torch.float64)
+#     dose = 1.0
+#     kernel_level = 15   #kernel数目
+#     start = time.time()
+#     output = simulate_image(mask, kernel, scale, dose, kernel_level)
+#     end = time.time()
+#     print(end-start)
     # print(output)
     # print((output1 == output2).all())
     # print(output)

ilt/utils/write_png.py
@@ -2,6 +2,7 @@ import torch
 
 from ilt.constant import TARGET_INTENSITY, MAX_DOSE, MIN_DOSE
 
+
 def write_image_file(image, dose, sizex=2048, sizey=2048):
     tempimg = torch.zeros([sizey, sizex], dtype=torch.int64)
     pngimg = torch.zeros([sizey, sizex, 4], dtype=torch.int64)
@@ -17,5 +18,5 @@ def write_image_file(image, dose, sizex=2048, sizey=2048):
         pngimg[:, :, 1] = 0
         pngimg[:, :, 0] = pngimg[:, :, 2] = tempimg/2
     pngimg[:, :, 3] = 127
-
+    print(torch.sum(pngimg[:,:,1] == 126))
     return pngimg

kernels/kernel.py
@@ -57,9 +57,9 @@ class Kernel(object):
             for j in range(5, len(data)//4-1, 2):
                 real_values = self.byte2float((data[4*j+3], data[4*j+2], data[4*j+1], data[4*j]))   # 逆排序是为了实现高位寻址->低位寻址
                 imag_values = self.byte2float((data[4*j+7], data[4*j+6], data[4*j+5], data[4*j+4]))
-                kernel.append(torch.tensor([real_values[0] + 1j*imag_values[0]], dtype=torch.complex64))
+                kernel.append(torch.tensor([real_values[0] + 1j*imag_values[0]], dtype=torch.complex128))
             kernels.append(kernel)
-        kernels = torch.tensor(kernels, dtype=torch.complex64)
+        kernels = torch.tensor(kernels, dtype=torch.complex128)
         kernels = (kernels.view((24, 35, 35))).permute(1, 2, 0)
         return kernels
 
@@ -68,7 +68,7 @@ class Kernel(object):
         self.combo_num = 9
         scales = torch.sqrt(self.scales)
         self.scales = torch.tensor([1], dtype=torch.float64)
-        combo_kernel = torch.zeros([self.knx, self.kny], dtype=torch.complex64).to(device)
+        combo_kernel = torch.zeros([self.knx, self.kny], dtype=torch.complex128).to(device)
         for i in range(self.combo_num):
             combo_kernel += scales[i] * self.kernels[:, :, i]
         self.kernels = combo_kernel